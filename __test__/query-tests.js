a6c01093a6f982861d35d50248a15dff94c47468
Luciano <passos.luciano@outlook.com>
2017-04-18 22:56:20 -0300

    query builder initial implementation

--- __test__/query-tests.js
+++ __test__/query-tests.js
@@ -3,21 +3,35 @@
 const test = require('blue-tape');
 const QueryBuilder = require('../lib/ormneo-query');
 
-test('A test test', (assert) => {
+test('Test condition to query method', (assert) => {
     let builder = new QueryBuilder();
-    let query = builder._conditionToQuery({'n.name' : {$eq : 'value'}});
+    let query = builder._conditionToQuery({ 'name': { $eq: 'value' } });
     assert.equal(query, 'n.name = \'value\'');
-    query = builder._conditionToQuery({'n.name' : {$eq: 'value', $lt: 'ab'} });
-    assert.equals(query, 'n.name = \'value\' AND n.name < \'ab\'');
-    query = builder._conditionToQuery({'n.age' : {$eq: 26 } });
-    assert.equals(query, 'n.age = 26');
-    query = builder._conditionToQuery({'n.age' : {$lte: 50 } });
-    assert.equals(query, 'n.age <= 50');
-    query = builder._conditionToQuery({'n.age' : {$ne: 50 } });
-    assert.equals(query, 'n.age != 50');
-    query = builder._conditionToQuery({'n.age' : {$gt: 50 } });
-    assert.equals(query, 'n.age > 50');
-    query = builder._conditionToQuery({'n.age' : {$gte: 50 } });
-    assert.equals(query, 'n.age >= 50');
+    query = builder._conditionToQuery({ 'name': { $eq: 'value', $lt: 'ab' } });
+    assert.equal(query, 'n.name = \'value\' AND n.name < \'ab\'');
+    query = builder._conditionToQuery({ 'age': { $eq: 26 } });
+    assert.equal(query, 'n.age = 26');
+    query = builder._conditionToQuery({ 'age': { $lte: 50 } });
+    assert.equal(query, 'n.age <= 50');
+    query = builder._conditionToQuery({ 'age': { $ne: 50 } });
+    assert.equal(query, 'n.age != 50');
+    query = builder._conditionToQuery({ 'age': { $gt: 50 } });
+    assert.equal(query, 'n.age > 50');
+    query = builder._conditionToQuery({ 'age': { $gte: 50 } });
+    assert.equal(query, 'n.age >= 50');
     assert.end();
 });
+
+test('Test query builder', (assert) => {
+    let queryBuilder = new QueryBuilder('Object');
+    let cypher = queryBuilder.and('name', { $eq: 'derp' })
+        .or('age', { $lt: 25 })
+        .and( 'last', { $eq: 'value' })
+        .build();
+    assert.equal(cypher, 'MATCH (n:Object) WHERE n.name = \'derp\' OR n.age < 25 AND n.last = \'value\' RETURN n');
+    queryBuilder.limit(25);
+    cypher = queryBuilder.build();
+    assert.equal(cypher, 'MATCH (n:Object) WHERE n.name = \'derp\' OR n.age < 25 AND n.last = \'value\' RETURN n LIMIT 25');
+
+    assert.end();
+}); 
