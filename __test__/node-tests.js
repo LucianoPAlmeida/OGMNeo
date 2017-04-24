8992487d8c8c61f8d5f1fcb4710b12c46ed87271
Luciano <passos.luciano@outlook.com>
2017-04-23 22:13:15 -0300

    begin implementing save and update nodes

--- __test__/node-tests.js
+++ __test__/node-tests.js
@@ -7,14 +7,31 @@ const ORMQueryBuilder = require('../lib/ormneo-query');
 
 ORMNeo.connect('neo4j', 'databasepass', 'localhost');
 
-// test('Test create node', (assert) => {
-//     ORMNeoNode.create({name: 'name'})
-// });
+test('Test create node', (assert) => {
+    ORMNeoNode.create({name: 'name', tes: 3},'test');
+    assert.end();
+});
 
-test('Test count', (assert) => {
-    ORMNeoNode.count( new ORMQueryBuilder('Object')).then(() => {
+test('Test update node', (assert) => {
+    ORMNeoNode.update({ id: 0, name: 'nameup', tes: 3});
+    assert.end();
+});
+
+test('Test delete NODE', (assert) => {
+    ORMNeoNode.delete({id: 1}).then((r)=> {
         assert.end();
     }).catch((error) => {
+        assert.error(error);
+        assert.end();
+    });
+});
+
+test('Test count', (assert) => {
+    ORMNeoNode.count( new ORMQueryBuilder('Object')).then((count) => {
+        assert.equal(count, 0);
+        assert.end();
+    }).catch((error) => {
+        assert.error(error);
         assert.end();
     });
 });
