d008e7198dfeaec4dd2bbc4802ad6fe7a65ad21c
Luciano <passos.luciano@outlook.com>
2017-05-03 00:13:18 -0300

    Fix modules

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-module.exports = require('./lib');
+module.exports = {
+    Connection: require('./lib/ormneo'),
+    ORMNeoNode: require('./lib/ormneo-node'),
+    QueryBuilder: require('./lib/ormneo-query'),
+    ORMNeoRelation: require('./lib/ormneo-relation')
+};
