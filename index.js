88877079630b2fbd4d193399c942d84819b41699
Luciano <passos.luciano@outlook.com>
2017-05-03 00:26:08 -0300

    update naming

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
     Connection: require('./lib/ormneo'),
     ORMNeoNode: require('./lib/ormneo-node'),
-    QueryBuilder: require('./lib/ormneo-query'),
+    ORMNeoQuery: require('./lib/ormneo-query'),
     ORMNeoRelation: require('./lib/ormneo-relation')
 };
