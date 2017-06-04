d5f202cf7ddd11678d301297fef003ccb4c28881
Luciano <passos.luciano@outlook.com>
2017-06-04 11:42:57 -0300

    error messages and tests enhancements.

--- __test__/relation-tests.js
+++ __test__/relation-tests.js
@@ -14,8 +14,8 @@ var nodes;
 var relations;
 
 test('Setup', (assert) => {
-    let values = [{name: 'Test1', value: 2}, {name: 'Test2', value: 4}];
-    let promises = values.map((node)=> { return OGMNeoNode.create(node, 'object');});
+    let values = [{ name: 'Test1', value: 2 }, { name: 'Test2', value: 4 }];
+    let promises = values.map((node) => { return OGMNeoNode.create(node, 'object'); });
     Promise.all(promises).then((all) => {
         nodes = all;
         assert.equal(nodes.length, 2);
@@ -26,8 +26,8 @@ test('Setup', (assert) => {
 test('Test CREATE relation', (assert) => {
     let node1 = nodes[0];
     let node2 = nodes[1];
-    let rel1 = OGMNeoRelation.relate(node1.id, 'relatedto', node2.id, {property: 'a'});
-    let rel2 = OGMNeoRelation.relate(node1.id, 'relatedto', node2.id,  {});
+    let rel1 = OGMNeoRelation.relate(node1.id, 'relatedto', node2.id, { property: 'a' });
+    let rel2 = OGMNeoRelation.relate(node1.id, 'relatedto', node2.id, {});
     Promise.all([rel1, rel2]).then((rels) => {
         assert.equal(rels.length, 2);
         let relation1 = rels[0];
@@ -40,16 +40,13 @@ test('Test CREATE relation', (assert) => {
         assert.equal(relation2.property, undefined);
         relations = rels;
         assert.end();
-    }).catch((error) => {
-        assert.fail('Fail with error');
-        assert.end();
     });
 });
 
 test('Test FAIL CREATE TYPE relation', (assert) => {
     let node1 = nodes[0];
     let node2 = nodes[1];
-    OGMNeoRelation.relate(node1.id, null, node2.id, {property: 'a'}).catch((error)=> {
+    OGMNeoRelation.relate(node1.id, null, node2.id, { property: 'a' }).catch((error) => {
         assert.equal(error.message, 'A relatioship type must be specified');
         assert.end();
     });
@@ -57,9 +54,7 @@ test('Test FAIL CREATE TYPE relation', (assert) => {
 
 test('Test FAIL CREATE IDS relation', (assert) => {
     let node2 = nodes[1];
-    OGMNeoRelation.relate('dasdsa', node2.id, 'type' , {property: 'a'}).then(()=> {
-        
-    }).catch((error)=> {
+    OGMNeoRelation.relate('dasdsa', node2.id, 'type', { property: 'a' }).catch((error) => {
         assert.equal(error.message, 'Ids from node must to be integers');
         assert.end();
     });
@@ -68,21 +63,26 @@ test('Test FAIL CREATE IDS relation', (assert) => {
 test('Test UPDATE relation', (assert) => {
     let relation = _.first(relations);
     assert.notEqual(relation, null);
-    OGMNeoRelation.update(relation.id, {newProperty: 'b', property: 'c'})
-    .then((updatedRel) => {
-        assert.equal(updatedRel.newProperty, 'b');
-        assert.equal(updatedRel.property, 'c');
-        assert.end();
-    }).catch((error)=> {
-        assert.fail('Fail with error');
-        assert.end();
-    });
+    OGMNeoRelation.update(relation.id, { newProperty: 'b', property: 'c' })
+        .then((updatedRel) => {
+            assert.equal(updatedRel.newProperty, 'b');
+            assert.equal(updatedRel.property, 'c');
+            assert.end();
+        });
+});
+
+test('Test FAIL UPDATE relation', (assert) => {
+    OGMNeoRelation.update('', { newProperty: 'b', property: 'c' })
+        .catch((error) => {
+            assert.equal(error.message, 'Relation id must to be integer');
+            assert.end();
+        });
 });
 
 test('Test FAIL UPDATE MANY', (assert) => {
     let node1 = nodes[0];
     let node2 = nodes[1];
-    OGMNeoRelation.updateMany({newProperty: 'new!!!'}, node1.id, node2.id, 'relatedto','').catch((error) => {
+    OGMNeoRelation.updateMany({ newProperty: 'new!!!' }, node1.id, node2.id, 'relatedto', '').catch((error) => {
         assert.equal(error.message, 'The propertiesFilter object must be an instance of OGMNeoWhere');
         assert.end();
     });
@@ -93,25 +93,25 @@ test('Test empty newProperties UPDATE MANY', (assert) => {
     let node2 = nodes[1];
     let where = OGMNeoWhere.create('property', { $eq: 'c' });
     OGMNeoRelation.updateMany({}, node1.id, node2.id, 'relatedto', null)
-    .then((updatedRelations) => {
-        assert.equal(updatedRelations.length, 0);
-        assert.end();
-    });
+        .then((updatedRelations) => {
+            assert.equal(updatedRelations.length, 0);
+            assert.end();
+        });
 });
 
 test('Test UPDATE MANY', (assert) => {
     let node1 = nodes[0];
     let node2 = nodes[1];
     let where = OGMNeoWhere.create('property', { $eq: 'c' });
-    OGMNeoRelation.updateMany({newProperty: 'new!!!'}, node1.id, node2.id, 'relatedto', where)
-    .then((updatedRelations) => {
-        assert.equal(updatedRelations.length, 1);
-        updatedRelations.forEach((relation) => {
-            assert.equal(relation.property, 'c');
-            assert.equal(relation.newProperty, 'new!!!');
+    OGMNeoRelation.updateMany({ newProperty: 'new!!!' }, node1.id, node2.id, 'relatedto', where)
+        .then((updatedRelations) => {
+            assert.equal(updatedRelations.length, 1);
+            updatedRelations.forEach((relation) => {
+                assert.equal(relation.property, 'c');
+                assert.equal(relation.newProperty, 'new!!!');
+            });
+            assert.end();
         });
-        assert.end();
-    });
 });
 
 test('Test FIND relations', (assert) => {
@@ -119,7 +119,7 @@ test('Test FIND relations', (assert) => {
     let node2 = nodes[1];
     let find1 = OGMNeoRelation.find(node1.id, node2.id, 'relatedto');
     let find2 = OGMNeoRelation.find(node1.id, node2.id, 'other');
-    let find3 = OGMNeoRelation.find(node1.id, node2.id, 'relatedto',new OGMNeoWhere('property', {$eq: 'c'}));
+    let find3 = OGMNeoRelation.find(node1.id, node2.id, 'relatedto', new OGMNeoWhere('property', { $eq: 'c' }));
 
     Promise.all([find1, find2, find3]).then((finds) => {
         assert.equal(finds[0].length, 2);
@@ -132,8 +132,12 @@ test('Test FIND relations', (assert) => {
         assert.equal(rel.type, 'relatedto');
         assert.equal(rel.property, 'c');
         assert.end();
-    }).catch((error)=> {
-        assert.fail('Fail with error');
+    });
+});
+
+test('Test FAIL FIND', (assert) => {
+    OGMNeoRelation.find({}, '', 'relatedto').catch((error)=> {
+        assert.equal(error.message, 'Both node ids have to be integers numbers');
         assert.end();
     });
 });
@@ -143,18 +147,22 @@ test('Test COUNT relations', (assert) => {
     let node2 = nodes[1];
     let count1 = OGMNeoRelation.count(node1.id, node2.id, 'relatedto');
     let count2 = OGMNeoRelation.count(node1.id, node2.id, 'other');
-    let count3 = OGMNeoRelation.count(node1.id, node2.id, 'relatedto', new OGMNeoWhere('property', {$eq: 'c'}));
+    let count3 = OGMNeoRelation.count(node1.id, node2.id, 'relatedto', new OGMNeoWhere('property', { $eq: 'c' }));
 
     Promise.all([count1, count2, count3]).then((counts) => {
         assert.equal(counts[0], 2);
         assert.equal(counts[1], 0);
         assert.equal(counts[2], 1);
         assert.end();
-    }).catch((error)=> {
-        assert.fail('Fail with error');
-        assert.end();
     });
 
+});
+
+test('Test FAIL COUNT', (assert) => {
+    OGMNeoRelation.count({}, '', 'relatedto').catch((error) => {
+        assert.equal(error.message, 'Both node ids have to be integers numbers');
+        assert.end();
+    });
 });
 
 test('Test EXISTS relations', (assert) => {
@@ -162,18 +170,22 @@ test('Test EXISTS relations', (assert) => {
     let node2 = nodes[1];
     let exists1 = OGMNeoRelation.exists(node1.id, node2.id, 'relatedto');
     let exists2 = OGMNeoRelation.exists(node1.id, node2.id, 'other');
-    let exists3 = OGMNeoRelation.exists(node1.id, node2.id, 'relatedto', new OGMNeoWhere('property', {$eq: 'c'}));
+    let exists3 = OGMNeoRelation.exists(node1.id, node2.id, 'relatedto', new OGMNeoWhere('property', { $eq: 'c' }));
 
     Promise.all([exists1, exists2, exists3]).then((exists) => {
         assert.equal(exists[0], true);
         assert.equal(exists[1], false);
         assert.equal(exists[2], true);
         assert.end();
-    }).catch((error)=> {
-        assert.fail('Fail with error');
-        assert.end();
     });
 
+});
+
+test('Test FAIL DELETE Relation', (assert) => {
+    OGMNeoRelation.deleteRelation({}).catch((error) => {
+        assert.equal(error.message, 'Relation id must to be an integer number');
+        assert.end();
+    });
 });
 
 test('TEST DELETE relation', (assert) => {
@@ -182,8 +194,12 @@ test('TEST DELETE relation', (assert) => {
     OGMNeoRelation.deleteRelation(rel.id).then((deletedRel) => {
         assert.equal(rel.id, deletedRel.id);
         assert.end();
-    }).catch((error) => {
-        assert.fail();
+    })
+});
+
+test('Test FAIL DELETE MANY', (assert) => {
+    OGMNeoRelation.deleteMany('', {}, 'relatedto').catch((error) => {
+        assert.equal(error.message, 'Both node ids have to be integers numbers');
         assert.end();
     });
 });
@@ -194,10 +210,7 @@ test('Test DELETE MANY relations', (assert) => {
     OGMNeoRelation.deleteMany(node1.id, node2.id, 'relatedto').then((deletedRels) => {
         assert.equal(deletedRels.length, 1);
         assert.end();
-    }).catch((error) => {
-        assert.fail();
-        assert.end();
-    });
+    })
 });
 
 test.onFinish(() => {
