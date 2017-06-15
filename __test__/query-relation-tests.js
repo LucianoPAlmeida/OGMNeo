e5a2f4470173010d325a29696a98b712b60520cf
Luciano <passos.luciano@outlook.com>
2017-06-15 20:54:17 -0300

    bug fixes on ogmneo-relation-query

---
+++ __test__/query-relation-tests.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const test = require('tape');
+const QueryBuilder = require('../lib/ogmneo-query');
+const OGMNeoWhere = require('../lib/ogmneo-where');
+const OGMNeoRelationQuery = require('../lib/ogmneo-relation-query');
+
+test('TEST RELATION QUERY CYPHER MATCH', (assert) => {
+    let query = OGMNeoRelationQuery.create('relation').startNode(23);
+    assert.equal(query.matchCypher(), 'MATCH p=(n1)-[r:relation]->(n2) WHERE ID(n1) = 23');
+    query = OGMNeoRelationQuery.create().startNode(2, 'label');
+    assert.equal(query.matchCypher(), 'MATCH p=(n1:label)-[r]->(n2) WHERE ID(n1) = 2');
+    query = OGMNeoRelationQuery.create('relation').startNode(2, 'label').endNode(43, 'label');
+    assert.equal(query.matchCypher(), 'MATCH p=(n1:label)-[r:relation]->(n2:label) WHERE ID(n1) = 2 AND ID(n2) = 43');
+    query = OGMNeoRelationQuery.create('relation').startNode(2, 'label').endNode(43, 'label').relationWhere(OGMNeoWhere.create('property', { $eq: 2 }).or('other', { $eq: 3 }));
+    assert.equal(query.matchCypher(), 'MATCH p=(n1:label)-[r:relation]->(n2:label) WHERE ID(n1) = 2 AND ID(n2) = 43 AND r.property = 2 OR r.other = 3');
+
+    query = OGMNeoRelationQuery.create('relation').startNode(2, 'label').endNode(43, 'label')
+        .relationWhere(OGMNeoWhere.create('property', { $eq: 2 }).or('other', { $eq: 3 }))
+        .startNodeWhere(OGMNeoWhere.create('name', { $eq: 'a' }));
+    assert.equal(query.matchCypher(), 'MATCH p=(n1:label)-[r:relation]->(n2:label) WHERE ID(n1) = 2 AND ID(n2) = 43 AND r.property = 2 OR r.other = 3 AND n1.name = \'a\'');
+    query = OGMNeoRelationQuery.create('relation').startNode(2, 'label').endNode(43, 'label')
+        .startNodeWhere(OGMNeoWhere.create('name', { $eq: 'a' }));
+    assert.equal(query.matchCypher(), 'MATCH p=(n1:label)-[r:relation]->(n2:label) WHERE ID(n1) = 2 AND ID(n2) = 43 AND n1.name = \'a\'');
+    query = OGMNeoRelationQuery.create()
+        .startNodeWhere(OGMNeoWhere.create('name', { $eq: 'a' })).endNodeWhere(OGMNeoWhere.create('name', {$eq: 'dsadsa'}));
+    assert.equal(query.matchCypher(), 'MATCH p=(n1)-[r]->(n2) WHERE n1.name = \'a\' AND n2.name = \'dsadsa\'');
+
+    query = OGMNeoRelationQuery.create('relation').startNode(2, 'label').endNode(43, 'label')
+        .relationWhere(OGMNeoWhere.create('property', { $eq: 2 }).or('other', { $eq: 3 }))
+        .startNodeWhere(OGMNeoWhere.create('name', { $eq: 'a' }))
+        .endNodeWhere(OGMNeoWhere.create('name', { $eq: 'b' }));
+    assert.equal(query.matchCypher(), 'MATCH p=(n1:label)-[r:relation]->(n2:label) WHERE ID(n1) = 2 AND ID(n2) = 43 AND r.property = 2 OR r.other = 3 AND n1.name = \'a\' AND n2.name = \'b\'');
+    assert.end();
+});
+
+test('TEST RELATION QUERY CYPHER QUERY', (assert) => {
+    let query = OGMNeoRelationQuery.create('relation').startNode(23).limit(20);
+    assert.equal(query.queryCypher(), 'MATCH p=(n1)-[r:relation]->(n2) WHERE ID(n1) = 23 RETURN r LIMIT 20');
+    assert.equal(query.queryPopulatedCypher(), 'MATCH p=(n1)-[r:relation]->(n2) WHERE ID(n1) = 23 RETURN r, n1, n2 LIMIT 20');
+    
+    query = OGMNeoRelationQuery.create('relation').startNode(23).returnStartNode('date').limit(20);
+    assert.equal(query.queryCypher(), 'MATCH p=(n1)-[r:relation]->(n2) WHERE ID(n1) = 23 RETURN r LIMIT 20');
+    assert.equal(query.queryPopulatedCypher(), 'MATCH p=(n1)-[r:relation]->(n2) WHERE ID(n1) = 23 RETURN r, n1.date, n2 LIMIT 20');
+    
+    query = OGMNeoRelationQuery.create('relation').startNode(23).returnStartNode('date').descOrderBy(['name','test']).limit(20);
+    assert.equal(query.queryCypher(), 'MATCH p=(n1)-[r:relation]->(n2) WHERE ID(n1) = 23 RETURN r ORDER BY r.name, r.test DESC LIMIT 20');
+    assert.equal(query.queryPopulatedCypher(), 'MATCH p=(n1)-[r:relation]->(n2) WHERE ID(n1) = 23 RETURN r, n1.date, n2 ORDER BY r.name, r.test DESC LIMIT 20');
+
+    query = OGMNeoRelationQuery.create('relation').startNode(23).returnStartNode('date').returnEndNode(['name', 'test']).descOrderBy(['name', 'test']).limit(20);
+    assert.equal(query.queryCypher(), 'MATCH p=(n1)-[r:relation]->(n2) WHERE ID(n1) = 23 RETURN r ORDER BY r.name, r.test DESC LIMIT 20');
+    assert.equal(query.queryPopulatedCypher(), 'MATCH p=(n1)-[r:relation]->(n2) WHERE ID(n1) = 23 RETURN r, n1.date, n2.name, n2.test ORDER BY r.name, r.test DESC LIMIT 20');
+
+    query = OGMNeoRelationQuery.create('relation').startNode(23).returnStartNode('date').returnEndNode(['name', 'test']).ascOrderBy(['name', 'test']);
+    assert.equal(query.queryCypher(), 'MATCH p=(n1)-[r:relation]->(n2) WHERE ID(n1) = 23 RETURN r ORDER BY r.name, r.test ASC');
+    assert.equal(query.queryPopulatedCypher(), 'MATCH p=(n1)-[r:relation]->(n2) WHERE ID(n1) = 23 RETURN r, n1.date, n2.name, n2.test ORDER BY r.name, r.test ASC');
+    assert.end();
+});
+
+
+
