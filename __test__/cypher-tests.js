18752ecee3215137434b38fc74c7bdf4726b5a25
Luciano <passos.luciano@outlook.com>
2017-07-03 22:57:12 -0300

    master: Remove wrong API and improve ogmneo cypher class.

--- __test__/cypher-tests.js
+++ __test__/cypher-tests.js
@@ -11,24 +11,47 @@ test('Test Connection', (assert) => {
     assert.end();
 });
 
-test('Test EXECUTE Transactional cypher', (assert) => {    
-   OGMNeoCypher.execute('MATCH (n) RETURN n', true).then((result) => {
+test('Test INVALID READ cypher', (assert) => {    
+    OGMNeoCypher.transactionalRead(4324).catch((error) => {
+         assert.notEqual(error,null);
+         assert.equal(error.message, 'Cypher statements must to be a string or a non empty string array');
+         assert.end();
+    });
+});
+
+test('Test EXECUTE READ Transactional cypher', (assert) => {    
+    OGMNeoCypher.transactionalRead('MATCH (n) RETURN n').then((result) => {
         assert.notEqual(result,null);
         assert.end();
    });
 });
 
-test('Test EXECUTE NOT Transactional cypher', (assert) => {    
-   OGMNeoCypher.execute('MATCH (n) RETURN COUNT(n)', false).then((result) => {
+test('Test EXECUTE READ MANY Transactional cypher', (assert) => {    
+    OGMNeoCypher.transactionalRead(['MATCH (n) RETURN COUNT(n)', 'MATCH (n) RETURN n']).then((result) => {
         assert.notEqual(result,null);
         assert.end();
    });
 });
 
-test('Test INVALID cypher', (assert) => {    
-   OGMNeoCypher.execute(4324).catch((error) => {
-        assert.notEqual(error,null);
-        assert.equal(error.message, 'Cypher query must to be a string statement');
-        assert.end();
-   });
+test('Test INVALID WRITE cypher', (assert) => {    
+    OGMNeoCypher.transactionalWrite(4324).catch((error) => {
+         assert.notEqual(error,null);
+         assert.equal(error.message, 'Cypher statements must to be a string or a non empty string array');
+         assert.end();
+    });
 });
+
+test('Test EXECUTE WRITE Transactional cypher', (assert) => {    
+    OGMNeoCypher.transactionalWrite('create (n {name: \'user1\'})').then((result) => {
+         assert.notEqual(result,null);
+         assert.end();
+    });
+ });
+ 
+ test('Test EXECUTE WRITE MANY Transactional cypher', (assert) => {    
+    OGMNeoCypher.transactionalWrite(['create (n {name: \'user1\'})', 'create (n {name: \'user2\'})']).then((result) => {
+         assert.notEqual(result,null);
+         assert.end();
+    });
+ });
+
