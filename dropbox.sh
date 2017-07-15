10e64b09f2bcc8f314fe47c692f58450fdf2a59c
Luciano <passos.luciano@outlook.com>
2017-07-14 23:08:50 -0300

    master: automatic script to deploy docs on getforge.com via dropbox folder

---
+++ dropbox.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm -rfv ~/.dropbox_uploader
+echo "OAUTH_ACCESS_TOKEN=$OAUTH_ACCESS_TOKEN" > ~/.dropbox_uploader
+
+curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o ~/dropbox_uploader.sh
+chmod +x ~/dropbox_uploader.sh
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    ~/dropbox_uploader.sh upload ./docs "ogmneo-docs"
+fi
+
+if [ "$TRAVIS_BRANCH" = "master" ] || [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    curl -s $UPLOADER_GENERATE_URL
+fi
+
+exit 0
