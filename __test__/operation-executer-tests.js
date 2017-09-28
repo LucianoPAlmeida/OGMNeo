6374af41c949784ae540ee0628d0ae96312e530c
Luciano <passos.luciano@outlook.com>
2017-09-27 22:33:11 -0300

    v1.0.0: Implementing first version of executeReadOperations and  executeWriteOperations on OGMNeoOperationExecuter

--- __test__/operation-executer-tests.js
+++ __test__/operation-executer-tests.js
@@ -3,6 +3,7 @@
 const test = require('tape');
 const { OGMNeoOperation, OGMNeoOperationBuilder } = require('../lib/ogmneo-operation');
 const OGMNeoOperationExecuter = require('../lib/ogmneo-operation-executer');
+const OGMNeoNode = require('../lib/ogmneo-node');
 const _ = require('lodash');
 
 test('Test Invalid Operation', (assert) => {    
@@ -32,4 +33,20 @@ test('Test invalid operation type on operations array', (assert) => {
         assert.equal(error.message, 'The parameter operations must be an array that contains only instances of ogmneo.Operation that have type : READ');
         assert.end();
     }); 
+});
+
+//Testing OGMNeoOperationExecuter.executeWriteOperations executor
+test('Test invalid operation type on operations array', (assert) => {    
+    let createUser1 = OGMNeoNode.createOperation({name: 'Ayrton Senna'}, 'Person');
+    let createUser2 = OGMNeoNode.createOperation({name: 'Alain Prost'}, 'Person');
+    
+    OGMNeoOperationExecuter.executeWriteOperations([createUser1, createUser2]).then((result) => {
+        let created1 = result[0];
+        let created2 = result[1];
+        assert.notEqual(created1.id, undefined);
+        assert.equal(created1.name, 'Ayrton Senna');
+        assert.notEqual(created2.id, undefined);
+        assert.equal(created2.name, 'Alain Prost');
+        assert.end();
+    });
 });
