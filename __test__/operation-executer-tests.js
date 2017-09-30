f863143dd9598d7cd6414ee203503d57aa32b968
Luciano <passos.luciano@outlook.com>
2017-09-29 22:33:06 -0300

    v1.0.0: Some files not added

--- __test__/operation-executer-tests.js
+++ __test__/operation-executer-tests.js
@@ -4,6 +4,8 @@ const test = require('tape');
 const { OGMNeoOperation, OGMNeoOperationBuilder } = require('../lib/ogmneo-operation');
 const OGMNeoOperationExecuter = require('../lib/ogmneo-operation-executer');
 const OGMNeoNode = require('../lib/ogmneo-node');
+const OGMNeo = require('../lib/ogmneo');
+
 const _ = require('lodash');
 
 test('Test Invalid Operation', (assert) => {    
@@ -13,40 +15,41 @@ test('Test Invalid Operation', (assert) => {
     }); 
 });
 
-test('Test invalid array of operations', (assert) => {    
-    OGMNeoOperationExecuter.executeReadOperations({}).catch((error) => {
-        assert.equal(error.message, 'The parameter operations must be an array');
-        assert.end();
-    }); 
-});
-
-test('Test invalid item on operations array', (assert) => {    
-    OGMNeoOperationExecuter.executeReadOperations([{}]).catch((error) => {
-        assert.equal(error.message, 'The parameter operations must be an array that contains only instances of ogmneo.Operation');
-        assert.end();
-    }); 
-});
-
-test('Test invalid operation type on operations array', (assert) => {    
-    let operation = OGMNeoOperationBuilder.create().type(OGMNeoOperation.WRITE).build();
-    OGMNeoOperationExecuter.executeReadOperations([operation]).catch((error) => {
-        assert.equal(error.message, 'The parameter operations must be an array that contains only instances of ogmneo.Operation that have type : READ');
-        assert.end();
-    }); 
-});
-
-//Testing OGMNeoOperationExecuter.executeWriteOperations executor
-test('Test invalid operation type on operations array', (assert) => {    
-    let createUser1 = OGMNeoNode.createOperation({name: 'Ayrton Senna'}, 'Person');
-    let createUser2 = OGMNeoNode.createOperation({name: 'Alain Prost'}, 'Person');
-    
-    OGMNeoOperationExecuter.executeWriteOperations([createUser1, createUser2]).then((result) => {
-        let created1 = result[0];
-        let created2 = result[1];
-        assert.notEqual(created1.id, undefined);
-        assert.equal(created1.name, 'Ayrton Senna');
-        assert.notEqual(created2.id, undefined);
-        assert.equal(created2.name, 'Alain Prost');
+//Testing OGMNeoOperationExecuter.write
+test('Test invalid write type on operation', (assert) => {    
+    OGMNeoOperationExecuter.write(null, (transaction) => {
+        //Nothing to test
+    }).catch((error) => {
+        assert.equal(error.message, 'You must provide a session object. See ogmneo.Connection.session()');
         assert.end();
     });
 });
+
+//Testing OGMNeoOperationExecuter.read
+test('Test invalid read type on operation', (assert) => {    
+    OGMNeoOperationExecuter.read(null, (transaction) => {
+        //Nothing to test
+    }).catch((error) => {
+        assert.equal(error.message, 'You must provide a session object. See ogmneo.Connection.session()');
+        assert.end();
+    });
+});
+
+
+    // let createUser1 = OGMNeoNode.createOperation({name: 'Ayrton Senna'}, 'Person');
+    // let createUser2 = OGMNeoNode.createOperation({name: 'Alain Prost'}, 'Person');
+    // OGMNeo.session().readTransaction((transaction) => {
+    //     let r = transaction.run('MATCH (n) RETURN n').then((result)=> {
+    //         console.log('result => ', result);
+    //     });
+    //     console.log(r);
+    // });
+    // OGMNeoOperationExecuter.executeWriteOperations([createUser1, createUser2]).then((result) => {
+    //     let created1 = result[0];
+    //     let created2 = result[1];
+    //     assert.notEqual(created1.id, undefined);
+    //     assert.equal(created1.name, 'Ayrton Senna');
+    //     assert.notEqual(created2.id, undefined);
+    //     assert.equal(created2.name, 'Alain Prost');
+    //     assert.end();
+    // });
