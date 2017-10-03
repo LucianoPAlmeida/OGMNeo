142a4b77876912a81f51f47b99a8190df09ebd17
Luciano <passos.luciano@outlook.com>
2017-10-03 20:07:17 -0300

    v1.0.0: Implement batching read and write operations.

--- __test__/operation-executer-tests.js
+++ __test__/operation-executer-tests.js
@@ -5,6 +5,8 @@ const { OGMNeoOperation, OGMNeoOperationBuilder } = require('../lib/ogmneo-opera
 const OGMNeoOperationExecuter = require('../lib/ogmneo-operation-executer');
 const OGMNeoNode = require('../lib/ogmneo-node');
 const OGMNeo = require('../lib/ogmneo');
+const OGMNeoQuery = require('../lib/ogmneo-query');
+const OGMNeoWhere = require('../lib/ogmneo-where');
 
 const _ = require('lodash');
 
@@ -15,31 +17,10 @@ test('Test Invalid Operation', (assert) => {
     }); 
 });
 
-//Testing OGMNeoOperationExecuter.write
-test('Test invalid write type on operation', (assert) => {    
-    OGMNeoOperationExecuter.write(null, (transaction) => {
-        //Nothing to test
-    }).catch((error) => {
-        assert.equal(error.message, 'You must provide a session object. See ogmneo.Connection.session()');
-        assert.end();
-    });
-});
-
-//Testing OGMNeoOperationExecuter.read
-test('Test invalid read type on operation', (assert) => {    
-    OGMNeoOperationExecuter.read(null, (transaction) => {
-        //Nothing to test
-    }).catch((error) => {
-        assert.equal(error.message, 'You must provide a session object. See ogmneo.Connection.session()');
-        assert.end();
-    });
-});
-
 //Testing OGMNeoOperationExecuter.read
 test('Test write type on operation', (assert) => {  
     let create = OGMNeoNode.createOperation({name: 'Ayrton Senna', carNumber: 12 }, 'Person');
-    let session = OGMNeo.session();  
-    OGMNeoOperationExecuter.write(session, (transaction) => {
+    OGMNeoOperationExecuter.write((transaction) => {
         return OGMNeoOperationExecuter.execute(create, transaction)
                                .then((created) => {
                                     assert.equal(created.name, 'Ayrton Senna');
@@ -50,27 +31,38 @@ test('Test write type on operation', (assert) => {
                                     return OGMNeoOperationExecuter.execute(update, transaction);
                                });
     }).then((result) => {
-        session.close();
         assert.equal(result.name, 'Ayrton Senna');
         assert.equal(result.carNumber, 1);
         assert.end();
     });
 });
 
-    // let createUser1 = OGMNeoNode.createOperation({name: 'Ayrton Senna'}, 'Person');
-    // let createUser2 = OGMNeoNode.createOperation({name: 'Alain Prost'}, 'Person');
-    // OGMNeo.session().readTransaction((transaction) => {
-    //     let r = transaction.run('MATCH (n) RETURN n').then((result)=> {
-    //         console.log('result => ', result);
-    //     });
-    //     console.log(r);
-    // });
-    // OGMNeoOperationExecuter.executeWriteOperations([createUser1, createUser2]).then((result) => {
-    //     let created1 = result[0];
-    //     let created2 = result[1];
-    //     assert.notEqual(created1.id, undefined);
-    //     assert.equal(created1.name, 'Ayrton Senna');
-    //     assert.notEqual(created2.id, undefined);
-    //     assert.equal(created2.name, 'Alain Prost');
-    //     assert.end();
-    // });
+test('Test batch write type operations', (assert) => {  
+    let createUser1 = OGMNeoNode.createOperation({name: 'Ayrton Senna'}, 'Person');
+    let createUser2 = OGMNeoNode.createOperation({name: 'Alain Prost'}, 'Person');
+
+    OGMNeoOperationExecuter.batchWriteOperations([createUser1, createUser2]).then((result) => {
+        let created1 = result[0];
+        let created2 = result[1];
+        assert.notEqual(created1.id, undefined);
+        assert.equal(created1.name, 'Ayrton Senna');
+        assert.notEqual(created2.id, undefined);
+        assert.equal(created2.name, 'Alain Prost');
+        assert.end();
+    });
+});
+
+test('Test batch read type operations', (assert) => {  
+    let query1 = OGMNeoNode.findOneOperation(OGMNeoQuery.create('Person').where(OGMNeoWhere.create('name', { $eq: 'Ayrton Senna' })));
+    let query2 = OGMNeoNode.findOneOperation(OGMNeoQuery.create('Person').where(OGMNeoWhere.create('name', { $eq: 'Alain Prost' })));
+
+    OGMNeoOperationExecuter.batchReadOperations([query1, query2]).then((result) => {
+        let found1 = result[0];
+        let found2 = result[1];
+        assert.notEqual(found1.id, undefined);
+        assert.equal(found1.name, 'Ayrton Senna');
+        assert.notEqual(found2.id, undefined);
+        assert.equal(found2.name, 'Alain Prost');
+        assert.end();
+    });
+});
