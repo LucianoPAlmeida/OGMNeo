ef37159acf4fa819c45ac18ccd583956c545f3db
Luciano Almeida <passos.luciano@outlook.com>
2019-02-04 21:46:41 -0200

    Normalize test descriptions

--- __test__/index-tests.js
+++ __test__/index-tests.js
@@ -3,42 +3,42 @@
 const test = require('tape');
 const OGMNeoIndex = require('../lib/ogmneo-index');
 
-test('Test create Index', (assert) => {    
+test('Test create an index', (assert) => {    
    OGMNeoIndex.create('object', ['name','test']).then((result) => {
         assert.notEqual(result, null);
         assert.end();
    });
 });
 
-test('Test create Index with string param', (assert) => {
+test('Test create index with a string param', (assert) => {
     OGMNeoIndex.create('object', 'string').then((result) => {
         assert.notEqual(result, null);
         assert.end();
     })
 });
 
-test('Test FAIL create Index with string param', (assert) => {
+test('Test FAIL to create an index with a string param', (assert) => {
     OGMNeoIndex.create('object', '').catch((error) => {
         assert.equals(error.message, 'You must provide and label and an array with at least one field name or a string name');
         assert.end();
     });
 });
 
-test('Test create Index fail', (assert) => {    
+test('Test FAIL to create index', (assert) => {    
     OGMNeoIndex.create(null, ['name','tes']).catch((error) => {
         assert.equals(error.message, 'You must provide and string as label param');
          assert.end();
     });
 });
 
-test('Test create index fail by parameters', (assert) => {
+test('Test FAIL to create index with invalid parameters', (assert) => {
     OGMNeoIndex.create('object', []).catch((error) => {
         assert.equals(error.message, 'You must provide and label and an array with at least one field name or a string name');
          assert.end();
     });
 });
 
-test('Test drop Index', (assert) => {    
+test('Test drop index', (assert) => {    
     OGMNeoIndex.drop('object', ['name','test']).then((result) => {
         assert.notEqual(result, null);
         assert.end();
